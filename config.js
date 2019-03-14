6c936e2465a935707964052cfcba44ae483e6b72
SoggyRamen <jj.harry2@gmail.com>
2019-03-14 12:02:00 -0400

    added bin file and updated config.js to remove oscars keys.

--- config.js
+++ config.js
@@ -2,10 +2,10 @@
 // Oscar Vanegas
 
 var config = {
-    consumer_key: 'zK5SYs3WsMWnq0RrrAGTLriH4',
-    consumer_secret: 'Q4f04KJQz1dGB1cWEtolPBGsFkAiCu4qeopxIJgAlUp9VvzhaQ',
-    access_token_key: '1103809024955424768-dV5SpyTGyhWcvGdpgI6b6tgVgX6z5k',
-    access_token_secret: 'ODcXZSS5TlIfxub3lfu9Xz4dK9Fa9MADfEKlXNtZimafo'
+    consumer_key: 'PUT KEYS HERE',
+    consumer_secret: 'PUT KEYS HERE',
+    access_token_key: 'PUT KEYS HERE',
+    access_token_secret: 'PUT KEYS HERE'
 };
 
 module.exports = config;
