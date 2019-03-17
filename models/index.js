126ad659f202998942fd7138fe1fb67cb956b53c
mike929 <mike929@gmail.com>
2019-03-17 14:22:59 -0400

    delete dup files

--- models/index.js
+++ models/index.js
@@ -9,15 +9,15 @@ var config = require(__dirname + "/../config/config.json")[env];
 var db = {};
 
 if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+  var sequelize = new Sequelize(process.env);
   // for use when deployed to heroku
   // var sequelize = new Sequelize(process.env.JAWDB_URL, {dialect: 'mysql'});
 } else {
   var sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
+    process.env.database,
+    process.username,
+    process.password
+    
   );
 }
 
